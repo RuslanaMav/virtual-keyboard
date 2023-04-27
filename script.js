40e911c341fdb9d335c6d3622c270f27b2f9b58d
RuslanaMav <ruslana.mav@yandex.ru>
2023-04-27 13:46:34 +0500

    feat: add ctrl, alt, change shift function, add change language

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ divKey.className = "keyboard";
 divMain.appendChild(divKey);
 
 let p = document.createElement("p");
-p.innerHTML = "Клавиатура создана в операционной системе MacOS <br> Для переключения языка комбинация: левыe control (ctrl) + option (alt)";
+p.innerHTML = "Клавиатура создана в операционной системе MacOS <br> Для переключения языка: левыe control (ctrl) + option (alt)";
 divMain.appendChild(p);
 
 textArea.focus();
@@ -25,7 +25,7 @@ const Eng = {
     3: ["`", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"]
 }
 
-const ru = {
+const Ru = {
     0: ["&gt;", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "-", "="],
     1: ["й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ"],
     2: ["ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "ё"],
@@ -46,7 +46,7 @@ for (let i = 0; i < 4; i++) {
     divKey.appendChild(keyRow);
     for (let j = 0; j < Eng[i].length; j++) {
         let key;
-        if (language) key = createKey(ru[i][j], "button " + nameKey[i][j]);
+        if (language) key = createKey(Ru[i][j], "button " + nameKey[i][j]);
         else key = createKey(Eng[i][j], "button " + nameKey[i][j]);
         createActive(key);
         keyRow.appendChild(key);
@@ -59,12 +59,13 @@ const k = document.getElementsByClassName("key-row");
 let backKey = createKey("Backspace", "buttonservice Backspace");
 k[0].appendChild(backKey);
 backKey.addEventListener('click', () => {
+    textArea.focus();
     let s = textArea.value;
     let position = getPosInRow(textArea);
     if (position !== 0) textArea.value = s.slice(0, position - 1) + s.slice(position);
-    textArea.setSelectionRange(position-1,position-1);
-    textArea.focus();
-})
+    else return;
+    textArea.setSelectionRange(position - 1, position - 1);
+});
 
 //Tab
 let tabKey = createKey("Tab", "buttonservice Tab");
@@ -75,7 +76,7 @@ tabKey.addEventListener('click', () => {
     let position = getPosInRow(textArea);
     if (s.length === 0 || getPosInRow(textArea) === s.length) textArea.value += "    ";
     else textArea.value = s.slice(0, position) + "    " + s.slice(position);
-    textArea.setSelectionRange(position+4,position+4);
+    textArea.setSelectionRange(position + 4, position + 4);
 });
 
 //Enter
@@ -89,8 +90,7 @@ enterKey.addEventListener('click', () => {
     if (getPosInRow(textArea) === s.length) textArea.value += "\n";
     else if (getPosInRow(textArea) === 0) textArea.value = "\n" + s;
     else textArea.value = s.slice(0, position) + "\n" + s.slice(position);
-    textArea.setSelectionRange(position+1,position+1);
-
+    textArea.setSelectionRange(position + 1, position + 1);
 });
 
 //CapsLock
@@ -135,34 +135,42 @@ k[3].appendChild(shift1Key);
 
 let shiftCount = false;
 shiftKey.addEventListener('click', () => {
-    textArea.focus();
-    if (!shiftCount) {
-        shiftKey.classList.add("active");
-        shiftCount = true;
-        shiftChange(shiftKey);
-    }
-    else {
-        shiftKey.classList.remove("active");
-        shiftCount = false;
-        shiftChangeBack();
-    }
-})
+    shiftActive(0);
+});
 
 shift1Key.addEventListener('click', () => {
-    textArea.focus();
-    if (!shiftCount) {
-        shift1Key.classList.add("active");
-        shiftCount = true;
-        shiftChange(shift1Key);
-    }
-    else {
-        shift1Key.classList.remove("active");
-        shiftCount = false;
-        shiftChangeBack();
-    }
-})
+    shiftActive(1);
+});
 
+let keyRow4 = document.createElement("div");
+keyRow4.className = "key-row";
+divKey.appendChild(keyRow4);
 
+//Control 
+let controlKey = createKey("Control", "buttonservice ControlLeft");
+keyRow4.appendChild(controlKey);
+
+//Alt 
+let altKey = createKey("Option", "buttonservice AltLeft");
+keyRow4.appendChild(altKey);
+
+const button = document.querySelectorAll("button");
+let array = [];
+for (let i of button) {
+    i.addEventListener("click", () => {
+        let a = i.className.slice(i.className.indexOf(" "))
+        if (a === " ControlLeft") array[0] = " ControlLeft";
+        else array.push(a);
+        if (array.length === 2) {
+            if (array[0] === " ControlLeft" && array[1] === " AltLeft") {
+                if (language) language = false;
+                else language = true;
+                changeLanguage();
+            }
+            array.length = 0;
+        }
+    })
+}
 
 
 function createKey(value, cl) {
@@ -179,7 +187,7 @@ function createActive(obj) {
         if (s.length === 0 || getPosInRow(textArea) === s.length) textArea.value += obj.innerText;
         else textArea.value = s.slice(0, position) + obj.innerText + s.slice(position);
         textArea.focus();
-        textArea.setSelectionRange(position+1,position+1);
+        textArea.setSelectionRange(position + 1, position + 1);
     });
 }
 
@@ -187,7 +195,7 @@ function getCaret(el) {
     if (el.selectionStart) return el.selectionStart;
     else if (document.selection) {
         let r = document.selection.createRange();
-        if (r == null) return 0;
+        if (r === null) return 0;
         let re = el.createTextRange(),
             rc = re.duplicate();
         re.moveToBookmark(r.getBookmark());
@@ -216,6 +224,22 @@ function changeCase() {
     }
 }
 
+function shiftActive(s) {
+    textArea.focus();
+    if (!shiftCount) {
+        if (s === 0) shiftKey.classList.add("active");
+        else shift1Key.classList.add("active");
+        shiftCount = true;
+        shiftChange();
+    }
+    else {
+        if (s === 0) shiftKey.classList.remove("active");
+        else shift1Key.classList.remove("active");
+        shiftCount = false;
+        shiftChangeBack();
+    }
+}
+
 function shiftChangeBack() {
     changeCase();
     for (let i = 0; i < r.length; i++)
@@ -228,7 +252,7 @@ function shiftChangeBack() {
     document.querySelector(".Slash").innerHTML = "/";
 }
 
-function shiftChange(s) {
+function shiftChange() {
     changeCase();
     for (let i = 0; i < r.length; i++)
         if (!language)
@@ -244,9 +268,40 @@ function shiftChange(s) {
         button[i].addEventListener('click', () => {
             if (shiftCount) {
                 shiftCount = false;
-                s.classList.remove("active");
+                shift1Key.classList.remove("active");
+                shiftKey.classList.remove("active");
                 shiftChangeBack();
             }
         });
     }
 }
+
+function changeLanguage() {
+    for (let i = 0; i < 4; i++) {
+        const button = k[i].querySelectorAll(".button");
+        for (let j = 0; j < button.length; j++) {
+            if (language) button[j].innerHTML = Ru[i][j];
+            if (!language) button[j].innerHTML = Eng[i][j];
+        }
+    }
+}
+
+(function (...codes) {
+    let set = new Set();
+    document.addEventListener('keydown', function (event) {
+        set.add(event.code);
+        for (let code of codes)
+            if (!set.has(code)) return;
+        set.clear();
+        if (language) language = false;
+        else language = true;
+        changeLanguage();
+        if (capsKey.classList.contains('active')) {
+            count = false;
+            changeCase();
+        }
+    });
+    document.addEventListener('keyup', function (event) {
+        set.delete(event.code);
+    });
+})("ControlLeft", "AltLeft")
